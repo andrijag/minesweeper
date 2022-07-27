4b8cc4e0042c8c64487eb74e25710aa9634ea150
Andrija Grozdanović <andrija.gro@gmail.com>
2022-07-27 18:22:16 +0200

    field construct get/set

--- src/games/minesweeper/field/Field.java
+++ src/games/minesweeper/field/Field.java
@@ -1,6 +1,8 @@
 package games.minesweeper.field;
 
+import games.minesweeper.field.state.Covered;
 import games.minesweeper.field.state.FieldState;
+import games.minesweeper.field.value.FieldNumber;
 import games.minesweeper.field.value.FieldValue;
 
 public class Field {
@@ -8,6 +10,23 @@ public class Field {
 	FieldState state;
 	
 	public Field() {
-		
+		value = new FieldNumber(0);
+		state = new Covered();
+	}
+	
+	public FieldValue getValue() {
+		return value;
+	}
+	
+	private void setValue(FieldValue value) {
+		this.value = value;
+	}
+	
+	public FieldState getState() {
+		return state;
+	}
+	
+	private void setState(FieldState state) {
+		this.state = state;
 	}
 }
