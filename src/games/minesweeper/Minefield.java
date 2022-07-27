cb649b1b2923557cabeeda0494eaeb85e7f0fc4e
Andrija Grozdanović <andrija.gro@gmail.com>
2022-07-27 18:57:03 +0200

    vector minefield incrementAdjecent

--- src/games/minesweeper/Minefield.java
+++ src/games/minesweeper/Minefield.java
@@ -6,7 +6,7 @@ public class Minefield {
 	int nRows;
 	int nColumns;
 	Field[][] matrix;
-	
+
 	public Minefield(int nRows, int nColumns) {
 		this.nRows = nRows;
 		this.nColumns = nColumns;
@@ -17,16 +17,28 @@ public class Minefield {
 			}
 		}
 	}
-	
+
 	public int getNRows() {
 		return nRows;
 	}
-	
+
 	public int getNColumns() {
 		return nColumns;
 	}
-	
+
 	public Field get(int i, int j) {
 		return matrix[i][j];
+	}
+
+	public void incrementAdjecent(int i, int j) {
+		int di;
+		int dj;
+		for (Vector vector : Vector.values()) {
+			di = vector.getI();
+			dj = vector.getJ();
+			if (0 <= i + di && i + di < nRows && 0 <= j + dj && j + dj < nColumns) {
+				matrix[i + di][j + dj].increment();
+			}
+		}
 	}
 }
---
+++ src/games/minesweeper/Vector.java
@@ -0,0 +1,21 @@
+package games.minesweeper;
+
+public enum Vector {
+	N(-1, 0), NE(-1, 1), E(0, 1), SE(1, 1), S(1, 0), SW(1, -1), W(0, -1), NW(-1, -1);
+
+	int i;
+	int j;
+
+	Vector(int i, int j) {
+		this.i = i;
+		this.j = j;
+	}
+
+	public int getI() {
+		return i;
+	}
+
+	public int getJ() {
+		return j;
+	}
+}
