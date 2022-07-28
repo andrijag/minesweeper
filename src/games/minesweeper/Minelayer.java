c3884144dad33a63186de235db40bce919bd1a1c
Andrija Grozdanović <andrija.gro@gmail.com>
2022-07-28 13:45:33 +0200

    naive layMine

--- src/games/minesweeper/Minelayer.java
+++ src/games/minesweeper/Minelayer.java
@@ -13,20 +13,14 @@ public class Minelayer {
 
 	public void layMines() {
 		Random rand = new Random();
-		double p = (double) nMines / (minefield.getNRows() * minefield.getNColumns() - 1);
 		int layedMines = 0;
-		while (true) {
-			for (int i = 0; i < minefield.getNRows(); i++) {
-				for (int j = 0; j < minefield.getNColumns(); j++) {
-					if (!minefield.isMine(i, j) && !minefield.isUncovered(i, j) && rand.nextDouble() < p) {
-						minefield.layMine(i, j);
-						minefield.incrementAdjecent(i, j);
-						layedMines++;
-						if (layedMines >= nMines){
-							return;
-						}
-					}
-				}
+		while (layedMines < nMines) {
+			int i = rand.nextInt(minefield.getNRows());
+			int j = rand.nextInt(minefield.getNColumns());
+			if (!minefield.isMine(i, j) && !minefield.isUncovered(i, j)) {
+				minefield.layMine(i, j);
+				minefield.incrementAdjecent(i, j);
+				layedMines++;
 			}
 		}
 	}
