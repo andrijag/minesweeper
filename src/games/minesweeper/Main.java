883c3ee7fa7256fc615fdace757174e3a7a185bb
Andrija Grozdanović <andrija.gro@gmail.com>
2022-08-04 14:19:39 +0200

    minelayer remove

--- src/games/minesweeper/Main.java
+++ src/games/minesweeper/Main.java
@@ -5,9 +5,10 @@ public class Main {
 		System.out.println("Minesweeper");
 		Minefield mf = new Minefield(10, 10);
 		Minelayer ml = new Minelayer(mf, 9);
+		Minesweeper ms = new Minesweeper(mf);
 		mf.uncover(0, 0);
+		ml.remove(mf.get(0, 0));
 		ml.layMines();
-		ml.scatterMines();
 		mf.updateValues();
 		System.out.println(mf);
 	}
