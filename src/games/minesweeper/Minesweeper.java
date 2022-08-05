c7f54e2153e303d1be46a96d4f12f86b7661df9b
Andrija Grozdanović <andrija.gro@gmail.com>
2022-08-05 09:48:08 +0200

    sweep flag expand

--- src/games/minesweeper/Minesweeper.java
+++ src/games/minesweeper/Minesweeper.java
@@ -6,4 +6,24 @@ public class Minesweeper {
 	public Minesweeper(Minefield minefield) {
 		this.minefield = minefield;
 	}
+
+	public void sweep(int i, int j) {
+		minefield.uncover(i, j);
+		if (minefield.get(i, j).equals(0)) {
+			for (Vector vector : Vector.values()) {
+				int di = vector.getI();
+				int dj = vector.getJ();
+				if (minefield.contains(i + di, j + dj) && !minefield.isUncovered(i + di, j + dj)) {
+					sweep(i + di, j + dj);
+				}
+			}
+		}
+	}
+
+	public void flag(int i, int j) {
+		minefield.flag(i, j);
+	}
+
+	public void expand(int i, int j) {
+	}
 }
