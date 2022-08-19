a55b1dcd94369b779f353fbb042c2467195a95e0
Andrija Grozdanović <andrija.gro@gmail.com>
2022-08-19 19:09:30 +0200

    delegate sweep to Field

--- src/games/minesweeper/Minesweeper.java
+++ src/games/minesweeper/Minesweeper.java
@@ -1,7 +1,5 @@
 package games.minesweeper;
 
-import games.minesweeper.field.Field;
-
 public class Minesweeper {
 	private Minefield minefield;
 
@@ -10,36 +8,7 @@ public class Minesweeper {
 	}
 
 	public void sweep(int i, int j) {
-		if (minefield.isUncovered(i, j)) {
-			expand(i, j);
-		} else {
-			uncover(i, j);
-		}
-	}
-
-	private void expand(int i, int j) {
-		Field field = minefield.get(i, j);
-		if (field.getNumber() == field.neighbourFlags()) {
-			for (Vector vector : Vector.values()) {
-				int di = vector.getI();
-				int dj = vector.getJ();
-				uncover(i + di, j + dj);
-			}
-		}
-	}
-
-	private void uncover(int i, int j) {
-		Field field = minefield.get(i, j);
-		field.uncover();
-		if (!field.isMine() && field.getNumber() == 0 && !field.isFlagged()) {
-			for (Vector vector : Vector.values()) {
-				int di = vector.getI();
-				int dj = vector.getJ();
-				if (minefield.contains(i + di, j + dj) && !minefield.isUncovered(i + di, j + dj)) {
-					uncover(i + di, j + dj);
-				}
-			}
-		}
+		minefield.sweep(i, j);
 	}
 
 	public void flag(int i, int j) {
