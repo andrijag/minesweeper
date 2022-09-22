7d93bc8355aa82e266f25fde5a5535233eff2408
Andrija Grozdanović <andrija.gro@gmail.com>
2022-09-22 17:46:54 +0200

    Winner state

---
+++ src/games/minesweeper/state/Winner.java
@@ -0,0 +1,21 @@
+package games.minesweeper.state;
+
+import games.minesweeper.Minesweeper;
+
+public class Winner extends GameState {
+	public Winner(Minesweeper game) {
+		super(game);
+	}
+
+	@Override
+	public void sweep(int i, int j) {
+	}
+
+	@Override
+	public void mark(int i, int j) {
+	}
+
+	@Override
+	public void chord(int i, int j) {
+	}
+}
