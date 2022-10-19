642c8496397ab190952013f7bdd2340ab5f59886
Andrija Grozdanović <andrija.gro@gmail.com>
2022-10-20 01:25:54 +0200

    Minelayer.scatterMines

--- src/main/java/games/minesweeper/gamestate/FirstMoveState.java
+++ src/main/java/games/minesweeper/gamestate/FirstMoveState.java
@@ -10,8 +10,8 @@ public class FirstMoveState extends GameState {
 	@Override
 	public void sweep(int i, int j) {
 		game.getStopwatch().start();
-//		game.getMinelayer().remove(game.getMinefield().get(i, j));
-//		game.getMinelayer().layMines();
+		game.getMinelayer().remove(game.getMinefield().get(i, j));
+		game.getMinelayer().scatterMines();
 		game.setState(new PlayingState(game));
 		game.sweep(i, j);
 	}
