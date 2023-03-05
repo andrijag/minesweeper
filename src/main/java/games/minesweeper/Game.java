9159805b9f3c3b0d2f9c161ead15648779be2906
Andrija Grozdanović <andrija.gro@gmail.com>
2023-03-05 04:02:32 +0100

    Add playing conditions

--- src/main/java/games/minesweeper/Game.java
+++ src/main/java/games/minesweeper/Game.java
@@ -10,7 +10,6 @@ public class Game extends Subject {
 	private Stopwatch stopwatch;
 	private Minefield minefield;
 	private Minelayer minelayer;
-	private Minesweeper minesweeper;
 	private GameState state;
 
 	public Game(int numberOfRows, int numberOfColumns, int numberOfMines) {
@@ -20,7 +19,6 @@ public class Game extends Subject {
 		stopwatch = new Stopwatch();
 		minefield = new Minefield(numberOfRows, numberOfColumns);
 		minelayer = new Minelayer(minefield);
-		minesweeper = new Minesweeper();
 		state = GameState.FIRST_MOVE;
 	}
 
@@ -37,32 +35,36 @@ public class Game extends Subject {
 	}
 
 	public void sweep(int i, int j) {
-		if (state == GameState.FIRST_MOVE) {
+		if (state == GameState.PLAYING) {
+			Minesweeper.sweep(minefield.getField(i, j));
+			evaluate();
+		} else if (state == GameState.FIRST_MOVE) {
 			stopwatch.start();
 			minelayer.remove(minefield.getField(i, j));
 			minelayer.scatterMines(numberOfMines);
 			state = GameState.PLAYING;
+			sweep(i, j);
 		}
-		Minesweeper.sweep(minefield.getField(i, j));
-		evaluate();
 	}
 
 	public void mark(int i, int j) {
-		if (state == GameState.FIRST_MOVE)
+		if (state == GameState.PLAYING)
+			Minesweeper.mark(minefield.getField(i, j));
+		else if (state == GameState.FIRST_MOVE)
 			stopwatch.start();
-		Minesweeper.mark(minefield.getField(i, j));
 	}
 
 	public void chord(int i, int j) {
-		Minesweeper.chord(minefield.getField(i, j));
-		evaluate();
+		if (state == GameState.PLAYING) {
+			Minesweeper.chord(minefield.getField(i, j));
+			evaluate();
+		}
 	}
 
 	public void restart() {
 		stopwatch.reset();
 		minefield = new Minefield(numberOfRows, numberOfColumns);
 		minelayer = new Minelayer(minefield);
-		minesweeper = new Minesweeper();
 		state = GameState.FIRST_MOVE;
 	}
 
